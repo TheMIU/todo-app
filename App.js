d497c3aad1e399a2754855905f75e250f33f2543
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2024-01-11 08:54:42 +0530

    use state hook

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('John');
+  const [person, setPerson] = useState({ name: 'John', age: 20 })
+
+  const clickHandler = () => {
+    setName('Tommy');
+    setPerson({ name: 'Tommy', age : 50 });
+  }
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.heading}>Hello world</Text>
+        <Text style={styles.heading}>{name}</Text>
+        <Text style={styles.heading}>person name is {person.name}, age is {person.age}</Text>
       </View>
       <View>
         <Text style={styles.paragraph}>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
           Praesentium sint facere assumenda natus nam atque. Saepe, quibusdam
           odio! Modi sed laborum totam nam reiciendis eos quia dolorem quaerat.
           Quos, deleniti.</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title='Change' onPress={clickHandler} />
       </View>
     </View>
   );
@@ -26,15 +38,21 @@ const styles = StyleSheet.create({
   },
 
   heading: {
-    backgroundColor: 'red',
+    backgroundColor: 'gray',
     padding: 20,
+    margin: 5,
+    textAlign:'center',
     fontWeight: 'bold',
-    color:'white'
+    color: 'white'
   },
 
-  paragraph:{
+  paragraph: {
     padding: 10,
-    textAlign:'center'
+    textAlign: 'center'
+  },
+
+  buttonContainer: {
+    marginTop: 20
   }
-  
+
 });
